02fd5f6cef622f19a674d1377a11d998d2b531a4
Aimee Wyld <aimee.eloise@mail.com>
2016-09-27 12:58:43 +0100

    Added two divs with css styling for full post and three most recent posts

--- article.php
+++ article.php
@@ -1,16 +1,48 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>A Wyld | Article</title>
-    <link rel="stylesheet" type="text/css" href="css/global.css">
-    <link rel="stylesheet" type="text/css" href="css/grid.css">
-    <link rel="stylesheet" type="text/css" href="css/nav.css">
-    <link rel="stylesheet" type="text/css" href="css/typography.css">
-    <link rel="stylesheet" type="text/css" href="css/header.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="icon" href="images/favicon.jpg" type="image/jpg" sizes="16x16">
-</head>
-<body></body>
+    <head>
+        <title>A Wyld | Article</title>
+        <link rel="stylesheet" type="text/css" href="css/global.css">
+        <link rel="stylesheet" type="text/css" href="css/grid.css">
+        <link rel="stylesheet" type="text/css" href="css/nav.css">
+        <link rel="stylesheet" type="text/css" href="css/typography.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="icon" href="images/favicon.jpg" type="image/jpg" sizes="16x16">
+    </head>
+    <body>
+        <div class="content">
+            <nav>
+                <a href="index.html">
+                    <div>
+                        <h2 class="titles">Home</h2>
+                    </div>
+                </a>
+                <a href="project-about-me.html">
+                    <div>
+                        <h2 class="titles">About</h2>
+                    </div>
+                </a>
+                <a href="project-contact.html">
+                    <div>
+                        <h2 class="titles">Contact</h2>
+                    </div>
+                </a>
+                <a href="project-portfolio.html">
+                    <div>
+                        <h2 class="titles">Portfolio</h2>
+                    </div>
+                </a>
+                <a href="blog.php" class="selected">
+                    <div>
+                        <h2 class="titles">Blog</h2>
+                    </div>
+                </a>
+            </nav>
+            <div class="article about-box border"></div>
+            <div class="article article2 about-box border"></div>
+        </div>
+    </body>
 </html>
 
 <?php
