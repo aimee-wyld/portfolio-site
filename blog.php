b61c9917f35eae3df5533f0ada437a1d13b6784f
Aimee Wyld <aimee.eloise@mail.com>
2016-09-27 12:52:52 +0100

    Added div with css styling for blog lists to go in

--- blog.php
+++ blog.php
@@ -10,7 +10,38 @@
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <link rel="icon" href="images/favicon.jpg" type="image/jpg" sizes="16x16">
     </head>
-    <body></body>
+    <body>
+        <div class="content">
+            <nav>
+                <a href="index.html">
+                    <div>
+                        <h2 class="titles">Home</h2>
+                    </div>
+                </a>
+                <a href="project-about-me.html">
+                    <div>
+                        <h2 class="titles">About</h2>
+                    </div>
+                </a>
+                <a href="project-contact.html">
+                    <div>
+                        <h2 class="titles">Contact</h2>
+                    </div>
+                </a>
+                <a href="project-portfolio.html">
+                    <div>
+                        <h2 class="titles">Portfolio</h2>
+                    </div>
+                </a>
+                <a href="blog.php" class="selected">
+                    <div>
+                        <h2 class="titles">Blog</h2>
+                    </div>
+                </a>
+            </nav>
+            <main id="portfolio-main" class="about-box border"></main>
+        </div>
+    </body>
 </html>
 
 <?php
