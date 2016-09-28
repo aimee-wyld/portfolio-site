0bb156d779e61fd38ffb10439b17dc8b8d928464
Aimee Wyld <aimee.eloise@mail.com>
2016-09-28 12:33:28 +0100

    Created php version of page and added nav bar

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--[if IE ]>
+<html class="ie">
+<![endif]-->
+<!--[if !IE]>-->
+<html>
+<!--<![endif]-->
+<head>
+	<title>A Wyld | Home</title>
+	<?php
+	require 'includes/links.php';
+	?>
+</head>
+<body id="home">
+	<div class="content">
+		<header>
+			<h2>Software Development | Portfolio</h2>
+		</header>
+		<main>
+			<h1><img src="images/name.png" alt="Aimee Wyld"></h1>
+		</main>
+		<?php
+		$selected_page = 'index';
+		require 'includes/nav.php'
+		?>
+	</div>
+</body>
+</html>
