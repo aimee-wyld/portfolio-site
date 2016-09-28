5a71ce4be38a39562ac48cd0c036ce640c4d553f
Aimee Wyld <aimee.eloise@mail.com>
2016-09-28 15:47:22 +0100

    swapped out code for the query_into_array function

--- article.php
+++ article.php
@@ -17,21 +17,24 @@ require 'includes/functions.php';
             ?>
             <div class="article about-box border">
                 <?php
+                $con = database_connect($database);
                 $url = $_GET['blog'];
-                $result = mysqli_query($con, 'SELECT `id`, `name`, `body`, `date_created` FROM `articles` WHERE `slug`="' . $url . '";');
-                $database = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                $result2 = mysqli_query($con, 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles` ORDER BY `date_created` DESC LIMIT 3;');
-                $recent_database = mysqli_fetch_all($result2, MYSQLI_ASSOC);
-                mysqli_close($con);
-                echo '<div class="blog-rows"><h1 class="blog-text">' . $database[0]['name'] . '</h1><h3>' . $database[0]['date_created'] . '</h3><p id="blog-desc" class="blog-text">' . $database[0]['body'] . '</p></div>';
+                $query_blog_article = 'SELECT `id`, `name`, `body`, `date_created` FROM `articles` WHERE `slug`="' . $url . '";';
+                $article_data = query_into_array($con, $query_blog_article);
+
+                echo '<div class="blog-rows"><h1 class="blog-text">' . $article_data[0]['name'] . '</h1><h3>' . $article_data[0]['date_created'] . '</h3><p id="blog-desc" class="blog-text">' . $article_data[0]['body'] . '</p></div>';
                 ?>
             </div>
             <div class="article article2 about-box border">
                 <h3 class="blog-text blog-title">Recent Blogs:</h3>
                 <?php
+                $con = database_connect($database);
+                $query_blog_recent = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles` ORDER BY `date_created` DESC LIMIT 3;';
+                $recent_data = query_into_array($con, $query_blog_recent);
+
                 $i = 0;
                 while ($i <= 2) {
-                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $recent_database[$i]['slug'] . '>' . $recent_database[$i]['name'] . '</a></h2><h5 class="blog-text">' . $recent_database[$i]['tags'] . '</h5><h5 class="blog-text">' . $recent_database[$i]['date_created'] . '</h5></div>';
+                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $recent_data[$i]['slug'] . '>' . $recent_data[$i]['name'] . '</a></h2><h5 class="blog-text">' . $recent_data[$i]['tags'] . '</h5><h5 class="blog-text">' . $recent_data[$i]['date_created'] . '</h5></div>';
                     $i++;
                 }
                 ?>
