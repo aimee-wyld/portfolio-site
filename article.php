755221f8f77be02cc620c9387e4c7583bef12850
Aimee Wyld <aimee.eloise@mail.com>
2016-09-28 12:08:33 +0100

    Added require link to links

--- article.php
+++ article.php
@@ -2,13 +2,9 @@
 <html>
     <head>
         <title>A Wyld | Article</title>
-        <link rel="stylesheet" type="text/css" href="css/global.css">
-        <link rel="stylesheet" type="text/css" href="css/grid.css">
-        <link rel="stylesheet" type="text/css" href="css/nav.css">
-        <link rel="stylesheet" type="text/css" href="css/typography.css">
-        <link rel="stylesheet" type="text/css" href="css/header.css">
-        <link rel="stylesheet" type="text/css" href="css/main.css">
-        <link rel="icon" href="images/favicon.jpg" type="image/jpg" sizes="16x16">
+        <?php
+        require 'includes/links.php';
+        ?>
     </head>
     <body>
         <div class="content">
