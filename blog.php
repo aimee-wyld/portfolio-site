d954a95ec7a49ba4c61b717d7027fbecb0b0cb02
Aimee Wyld <aimee.eloise@mail.com>
2016-09-28 15:46:42 +0100

    swapped out code for the query_into_array function

--- blog.php
+++ blog.php
@@ -17,13 +17,13 @@ require 'includes/functions.php';
             ?>
             <main id="blog" class="about-box border">
                 <?php
-                $result = mysqli_query($con, 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles`;');
-                $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                mysqli_close($con);
+                $con = database_connect($database);
+                $query_bloglist = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles`;';
+                $bloglist_data = query_into_array($con, $query_bloglist);
 
                 $i = 0;
-                foreach ($data[$i] as $key => $value) {
-                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $data[$i]['slug'] . '>' . $data[$i]['name'] . '</a></h2><p id="blog-desc" class="blog-text">' . $data[$i]['desc'] . '<a href=article.php?blog=' . $data[$i]['slug'] . '><i>Read more</i>' . '</a></p><h5 class="blog-text">' . $data[$i]['tags'] . '</h5><h5 class="blog-text">' . $data[$i]['date_created'] . '</h5></div>';
+                foreach ($bloglist_data[$i] as $key => $value) {
+                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $bloglist_data[$i]['slug'] . '>' . $bloglist_data[$i]['name'] . '</a></h2><p id="blog-desc" class="blog-text">' . $bloglist_data[$i]['desc'] . '<a href=article.php?blog=' . $bloglist_data[$i]['slug'] . '><i>Read more</i>' . '</a></p><h5 class="blog-text">' . $bloglist_data[$i]['tags'] . '</h5><h5 class="blog-text">' . $bloglist_data[$i]['date_created'] . '</h5></div>';
                     $i ++;
                 }
                 ?>
