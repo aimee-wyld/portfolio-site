bbadfb9258b8b485bc8cba45de44db0910064a9e
Aimee Wyld <aimee.eloise@mail.com>
2016-09-28 14:47:12 +0100

    Swapped out some of the code for the database_connect function

--- blog.php
+++ blog.php
@@ -1,3 +1,6 @@
+<?php
+require 'includes/functions.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -14,18 +17,13 @@
             ?>
             <main id="blog" class="about-box border">
                 <?php
-                $con = mysqli_connect("192.168.20.56","root","","my_blog");
-                if (mysqli_connect_errno()) {
-                    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                }
-
                 $result = mysqli_query($con, 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles`;');
-                $database = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
                 mysqli_close($con);
 
                 $i = 0;
-                foreach ($database[$i] as $key => $value) {
-                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $database[$i]['slug'] . '>' . $database[$i]['name'] . '</a></h2><p id="blog-desc" class="blog-text">' . $database[$i]['desc'] . '<a href=article.php?blog=' . $database[$i]['slug'] . '><i>Read more</i>' . '</a></p><h5 class="blog-text">' . $database[$i]['tags'] . '</h5><h5 class="blog-text">' . $database[$i]['date_created'] . '</h5></div>';
+                foreach ($data[$i] as $key => $value) {
+                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $data[$i]['slug'] . '>' . $data[$i]['name'] . '</a></h2><p id="blog-desc" class="blog-text">' . $data[$i]['desc'] . '<a href=article.php?blog=' . $data[$i]['slug'] . '><i>Read more</i>' . '</a></p><h5 class="blog-text">' . $data[$i]['tags'] . '</h5><h5 class="blog-text">' . $data[$i]['date_created'] . '</h5></div>';
                     $i ++;
                 }
                 ?>
