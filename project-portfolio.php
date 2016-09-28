eb6768ce41044f6925c0661cc85ad1b8a5f89450
Aimee Wyld <aimee.eloise@mail.com>
2016-09-28 12:36:59 +0100

    created php version of page and added nav bar

---
+++ project-portfolio.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--[if IE ]>
+<html class="ie">
+<![endif]-->
+<!--[if !IE]>-->
+<html>
+<!--<![endif]-->
+<head>
+	<title>A Wyld | Portfolio</title>
+	<?php
+	require 'includes/links.php';
+	?>
+</head>
+<body>
+	<div class="content">
+		<?php
+		$selected_page = 'portfolio';
+		require 'includes/nav.php'
+		?>
+		<main id="portfolio-main">
+			<div class="border background col-1 corner1">
+				<img src="images/elephant.jpg" alt="elephant" class="border project-info">
+			</div>
+			<div class="border background project-desc corner2">
+				<h3>Project Title</h3>
+				<p class="border project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor est eu elit tincidunt, lobortis viverra eros gravida. In hac habitasse platea dictumst. Cras ornare vel sapien vel dapibus. Maecenas eu dui at tortor commodo cursus eu non libero. Aliquam erat volutpat. Aliquam erat volutpat. Sed vel massa diam. Donec porttitor eros sit amet commodo tristique. Nulla vel porttitor purus. Donec sit amet luctus nisi. Etiam vel dapibus massa. Curabitur lacinia laoreet rutrum.</p>
+			</div>
+			<div class="border background col-1">
+				<h3>Project Title</h3>
+				<p class="border project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor est eu elit tincidunt, lobortis viverra eros gravida. In hac habitasse platea dictumst. Cras ornare vel sapien vel dapibus. Maecenas eu dui at tortor commodo cursus eu non libero. Aliquam erat volutpat. Aliquam erat volutpat. Sed vel massa diam. Donec porttitor eros sit amet commodo tristique. Nulla vel porttitor purus. Donec sit amet luctus nisi. Etiam vel dapibus massa. Curabitur lacinia laoreet rutrum.</p>
+			</div>
+			<div class="border background">
+				<img src="images/bird.jpg" alt="bird" class="border project-info">
+			</div>
+			<div class="border background col-1">
+				<img src="images/giraffe.jpg" alt="giraffe" class="border project-info">
+			</div>
+			<div class="border background project-desc">
+				<h3>Project Title</h3>
+				<p class="border project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor est eu elit tincidunt, lobortis viverra eros gravida. In hac habitasse platea dictumst. Cras ornare vel sapien vel dapibus. Maecenas eu dui at tortor commodo cursus eu non libero. Aliquam erat volutpat. Aliquam erat volutpat. Sed vel massa diam. Donec porttitor eros sit amet commodo tristique. Nulla vel porttitor purus. Donec sit amet luctus nisi. Etiam vel dapibus massa. Curabitur lacinia laoreet rutrum.</p>
+			</div>
+			<div class="border background col-1">
+				<h3>Project Title</h3>
+				<p class="border project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor est eu elit tincidunt, lobortis viverra eros gravida. In hac habitasse platea dictumst. Cras ornare vel sapien vel dapibus. Maecenas eu dui at tortor commodo cursus eu non libero. Aliquam erat volutpat. Aliquam erat volutpat. Sed vel massa diam. Donec porttitor eros sit amet commodo tristique. Nulla vel porttitor purus. Donec sit amet luctus nisi. Etiam vel dapibus massa. Curabitur lacinia laoreet rutrum.</p>
+			</div>
+			<div class="border background">
+				<img src="images/polar-bear.jpeg" alt="polar bear" class="border project-info">
+			</div>
+			<div class="border background col-1 alt corner3">
+				<img src="images/fish-face.jpeg" alt="fish" class="border project-info">
+			</div>
+			<div class="border background project-desc alt corner4">
+				<h3>Project Title</h3>
+				<p class="border project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor est eu elit tincidunt, lobortis viverra eros gravida. In hac habitasse platea dictumst. Cras ornare vel sapien vel dapibus. Maecenas eu dui at tortor commodo cursus eu non libero. Aliquam erat volutpat. Aliquam erat volutpat. Sed vel massa diam. Donec porttitor eros sit amet commodo tristique. Nulla vel porttitor purus. Donec sit amet luctus nisi. Etiam vel dapibus massa. Curabitur lacinia laoreet rutrum.</p>
+			</div>
+		</main>
+	</div>
+</body>
+</html>
