e1e6ded7347215cada331d560f82f2f34fb8db64
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 13:32:57 +0100

    Separated select queries for readability

--- article.php
+++ article.php
@@ -18,7 +18,9 @@ require 'includes/functions.php';
             <div class="article about-box border">
                 <?php
                 $con = database_connect($database);
-                $query_blog_article = 'SELECT `id`, `name`, `body`, `date_created` FROM `articles` WHERE `slug`="' . $_GET['blog'] . '";';
+                $query_blog_article = 'SELECT `id`, `name`, `body`, `date_created` 
+                                       FROM `articles` 
+                                       WHERE `slug`="' . $_GET['blog'] . '";';
                 $article_data = query_into_array($con, $query_blog_article);
                 echo '<div class="blog-rows">' .
                             '<h2 class="blog-text">' . $article_data[0]['name'] . '</h2>' .
@@ -31,7 +33,10 @@ require 'includes/functions.php';
                 <h3 class="blog-text blog-title">Most Recent Blogs:</h3>
                 <?php
                 $con = database_connect($database);
-                $query_blog_recent = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles` ORDER BY `date_created` DESC LIMIT 3;';
+                $query_blog_recent = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` 
+                                      FROM `articles` 
+                                      ORDER BY `date_created` 
+                                      DESC LIMIT 3;';
                 $recent_data = query_into_array($con, $query_blog_recent);
                 echo data_iterator($recent_data);
                 ?>
