cdd1946cc2828baf0bcb6322abb1307269b44fff
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 09:19:32 +0100

    Created good unit test and tested

---
+++ includes/test/functions.php
@@ -0,0 +1,25 @@
+<?php
+require('../functions.php');
+class StackTest extends PHPUNIT_Framework_Testcase {
+
+public function test_data_iterator_good() {
+    $array = [
+        ['slug' => 'bla-bla', 'name' => 'bloo-blaa', 'desc' => 'blaa-bloo', 'tags' => '(bla) (blo)', 'date_created' => '5th June'],
+        ['slug' => 'bla-bla', 'name' => 'bloo-blaa', 'desc' => 'blaa-bloo', 'tags' => '(bla) (blo)', 'date_created' => '5th June'],
+        ['slug' => 'bla-bla', 'name' => 'bloo-blaa', 'desc' => 'blaa-bloo', 'tags' => '(bla) (blo)', 'date_created' => '5th June'],
+    ];
+    data_iterator($array);
+}
+
+//public function test_data_iterator_bad() {
+//
+//}
+//
+//public function test_data_iterator_malformed() {
+//
+//}
+
+}
+?>
+
+
