f0bfe663d38dc11b0995b8ed162e465a57016d1e
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 17:10:39 +0100

    corrected hrefs to include ""

--- includes/test/functions.php
+++ includes/test/functions.php
@@ -13,20 +13,20 @@ class StackTest extends PHPUNIT_Framework_Testcase {
         ];
         $data = data_iterator($array);
         $this->assertEquals($data, '<div class="blog-rows">' .
-                                        '<h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3>' .
-                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p>' .
+                                        '<h3 class="blog-text"><a href="article.php?blog=bla-bla>bloo-blaa"</a></h3>' .
+                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href="article.php?blog=bla-bla"><i>Read more</i></a></p>' .
                                         '<h5 class="blog-text">(bla) (blo)</h5>' .
                                         '<h5 class="blog-text">5th June</h5>' .
                                     '</div>' .
                                     '<div class="blog-rows">' .
-                                        '<h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3>' .
-                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p>' .
+                                        '<h3 class="blog-text"><a href="article.php?blog=bla-bla>bloo-blaa"</a></h3>' .
+                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href="article.php?blog=bla-bla"><i>Read more</i></a></p>' .
                                         '<h5 class="blog-text">(bla) (blo)</h5>' .
                                         '<h5 class="blog-text">5th June</h5>' .
                                     '</div>' .
                                     '<div class="blog-rows">' .
-                                        '<h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3>' .
-                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p>' .
+                                        '<h3 class="blog-text"><a href="article.php?blog=bla-bla>bloo-blaa"</a></h3>' .
+                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href="article.php?blog=bla-bla"><i>Read more</i></a></p>' .
                                         '<h5 class="blog-text">(bla) (blo)</h5>' .
                                         '<h5 class="blog-text">5th June</h5>' .
                                     '</div>');
