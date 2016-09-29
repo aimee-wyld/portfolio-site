4fa846854fc0ba0b934954543842a97bb3b57306
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 13:28:46 +0100

    Formatted data output in first unit test for clearer reading

--- includes/test/functions.php
+++ includes/test/functions.php
@@ -9,7 +9,24 @@ class StackTest extends PHPUNIT_Framework_Testcase {
             ['slug' => 'bla-bla', 'name' => 'bloo-blaa', 'desc' => 'blaa-bloo', 'tags' => '(bla) (blo)', 'date_created' => '5th June'],
         ];
         $data = data_iterator($array);
-        $this->assertEquals($data, '<div class="blog-rows"><h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3><p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p><h5 class="blog-text">(bla) (blo)</h5><h5 class="blog-text">5th June</h5></div><div class="blog-rows"><h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3><p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p><h5 class="blog-text">(bla) (blo)</h5><h5 class="blog-text">5th June</h5></div><div class="blog-rows"><h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3><p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p><h5 class="blog-text">(bla) (blo)</h5><h5 class="blog-text">5th June</h5></div>');
+        $this->assertEquals($data, '<div class="blog-rows">' .
+                                        '<h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3>' .
+                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p>' .
+                                        '<h5 class="blog-text">(bla) (blo)</h5>' .
+                                        '<h5 class="blog-text">5th June</h5>' .
+                                    '</div>' .
+                                    '<div class="blog-rows">' .
+                                        '<h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3>' .
+                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p>' .
+                                        '<h5 class="blog-text">(bla) (blo)</h5>' .
+                                        '<h5 class="blog-text">5th June</h5>' .
+                                    '</div>' .
+                                    '<div class="blog-rows">' .
+                                        '<h3 class="blog-text"><a href=article.php?blog=bla-bla>bloo-blaa</a></h3>' .
+                                        '<p id="blog-desc" class="blog-text">blaa-bloo<a href=article.php?blog=bla-bla><i>Read more</i></a></p>' .
+                                        '<h5 class="blog-text">(bla) (blo)</h5>' .
+                                        '<h5 class="blog-text">5th June</h5>' .
+                                    '</div>');
 }
 
     public function test_data_iterator_bad() {
