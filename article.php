85cb53807cb563ac65c79d2ecd96082027e3bd71
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 14:54:17 +0100

    added return to blog list link

--- article.php
+++ article.php
@@ -23,7 +23,7 @@ require 'includes/functions.php';
                                        WHERE `slug`="' . $_GET['blog'] . '";';
                 $article_data = query_into_array($con, $query_blog_article);
                 echo '<div class="blog-rows">' .
-                            '<h2 class="blog-text">' . $article_data[0]['name'] . '</h2>' .
+                            '<h2 class="blog-text underline">' . $article_data[0]['name'] . '</h2>' .
                             '<h5 class="blog-text">' . $article_data[0]['date_created'] . '</h5>' .
                             '<p id="blog-desc" class="blog-text">' . $article_data[0]['body'] . '</p>' .
                       '</div>';
@@ -31,6 +31,7 @@ require 'includes/functions.php';
             </div>
             <div class="article article2 about-box border">
                 <h3 class="blog-text blog-title">Most Recent Blogs:</h3>
+                <h3 class="blog-text blog-title blog-link"><a href="blog.php">Back to blog list</a></h3>
                 <?php
                 $con = database_connect($database);
                 $query_blog_recent = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` 
