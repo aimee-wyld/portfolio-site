0a96d8427e293a75cdc380df96915a2b442380f7
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 09:57:30 +0100

    Updated from procedural to functional

--- blog.php
+++ blog.php
@@ -20,12 +20,7 @@ require 'includes/functions.php';
                 $con = database_connect($database);
                 $query_bloglist = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` FROM `articles`;';
                 $bloglist_data = query_into_array($con, $query_bloglist);
-
-                $i = 0;
-                foreach ($bloglist_data[$i] as $key => $value) {
-                    echo '<div class="blog-rows"><h2 class="blog-text"><a href=article.php?blog=' . $bloglist_data[$i]['slug'] . '>' . $bloglist_data[$i]['name'] . '</a></h2><p id="blog-desc" class="blog-text">' . $bloglist_data[$i]['desc'] . '<a href=article.php?blog=' . $bloglist_data[$i]['slug'] . '><i>Read more</i>' . '</a></p><h5 class="blog-text">' . $bloglist_data[$i]['tags'] . '</h5><h5 class="blog-text">' . $bloglist_data[$i]['date_created'] . '</h5></div>';
-                    $i ++;
-                }
+                echo data_iterator($bloglist_data);
                 ?>
             </main>
         </div>
