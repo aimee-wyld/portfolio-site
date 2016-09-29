ef79091e21aa48236dd3ee4c656195fcbe0eb993
Aimee Wyld <aimee.eloise@mail.com>
2016-09-29 13:20:44 +0100

    Formatted query output for clearer reading

--- article.php
+++ article.php
@@ -20,7 +20,11 @@ require 'includes/functions.php';
                 $con = database_connect($database);
                 $query_blog_article = 'SELECT `id`, `name`, `body`, `date_created` FROM `articles` WHERE `slug`="' . $_GET['blog'] . '";';
                 $article_data = query_into_array($con, $query_blog_article);
-                echo '<div class="blog-rows"><h2 class="blog-text">' . $article_data[0]['name'] . '</h2><h5 class="blog-text">' . $article_data[0]['date_created'] . '</h5><p id="blog-desc" class="blog-text">' . $article_data[0]['body'] . '</p></div>';
+                echo '<div class="blog-rows">' .
+                            '<h2 class="blog-text">' . $article_data[0]['name'] . '</h2>' .
+                            '<h5 class="blog-text">' . $article_data[0]['date_created'] . '</h5>' .
+                            '<p id="blog-desc" class="blog-text">' . $article_data[0]['body'] . '</p>' .
+                      '</div>';
                 ?>
             </div>
             <div class="article article2 about-box border">
