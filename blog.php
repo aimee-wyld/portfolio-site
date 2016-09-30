e4f0bb3af17fef612ad2fe7e55d500551f247cca
Aimee Wyld <aimee.eloise@mail.com>
2016-09-30 10:10:59 +0100

    added comments to php

--- blog.php
+++ blog.php
@@ -17,10 +17,17 @@ require 'includes/functions.php';
             ?>
             <main id="blog" class="about-box border">
                 <?php
+                //uses function to connect to the specified database and assigns connection information to a variable.
                 $con = database_connect($database);
+
+                //assigns a query for the overall blog list to a variable.
                 $query_bloglist = 'SELECT `id`, `name`, `slug`, `desc`, `tags`, `date_created` 
                                    FROM `articles`';
+
+                //uses function to perform query on database and return the data as an array.
                 $bloglist_data = query_into_array($con, $query_bloglist);
+
+                //uses function to iterate over the array and echoes out specified information concatenated with html.
                 echo data_iterator($bloglist_data);
                 ?>
             </main>
